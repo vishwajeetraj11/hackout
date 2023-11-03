1e4d02b027424eb1e4c1e6b6ce07291a16817a69
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2023-11-03 22:54:02 +0530

    APIs

---
+++ src/app/api/generateIndex/route.ts
@@ -0,0 +1,13 @@
+import { generateIndex } from "@/lib/gpt";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request, res: Response) => {
+    try {
+        const body = await req.json();
+        const chapterTitles = await generateIndex(body.title, body.num);
+        return NextResponse.json({ status: true, message: 'Index Generated.', chapterTitles }, { status: 200 });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({ status: false, message: 'Something went worng!' }, { status: 400 });
+    }
+}
