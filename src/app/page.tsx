6b6e2fb6f4f6597d71c6faec71ca05634cb86a74
rishu <divyanshkumarworks@gmail.com>
2023-11-04 11:35:54 +0530

    drag and drop added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,54 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { useMutation } from "@tanstack/react-query";
 
+import { closestCenter, DndContext } from "@dnd-kit/core";
+import { SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 type Props = {};
+
+// const SortableChapter = ({chapter}) => {
+//   const { 
+//     attributes, 
+//     listeners, 
+//     setNodeRef, 
+//     transform, 
+//     transition 
+//   } = useSortable({ id: chapter })
+//   const style = {
+//     transition,
+//     transform: CSS.Transform.toString(transform),
+//   };
+
+//   return (
+//       <div ref={setNodeRef} style={style} {...attributes} {...listeners} className="mb-3 font-normal text-gray-700 dark:text-gray-400y">
+//         {chapter}
+//       </div>  
+//     )
+// }
 
 const GenerateIndex = (props: Props) => {
   const [title, setTitle] = React.useState("");
+  const [chapters, setChapters] = React.useState([
+    {
+        "content": "Unicorn Tales"
+    },
+    {
+        "content": "Dragon Dreams"
+    },
+    {
+        "content": "A Clash of Fantasies"
+    },
+    {
+        "content": "Mystical Creatures Unite"
+    },
+    {
+        "content": "The Legend Lives On"
+    },
+    {
+        "content": "An Enchanted Journey"
+    }
+]);
 
   const { data, mutateAsync } = useMutation({
     mutationKey: ["Index", title],
@@ -30,6 +74,31 @@ const GenerateIndex = (props: Props) => {
     );
   };
 
+  // const loadTitleChapters = async () => {
+  //   try {
+  //     const response = await fetch('/ebooks.json');
+  //     if (!response.ok) {
+  //       throw new Error('Failed to fetch data');
+  //     }
+  //     const jsonData = await response.json();
+  //     const ebooks = jsonData["ebooks"]
+  //     const ebook = jsonData.ebooks.find((ebook) => ebook.title === title);
+  //     if (ebook) {
+  //       console.log(ebook)
+  //       setChapters(ebook.chapters);
+  //     }
+  //     else {
+  //       console.log("book not found")
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // const onDragEnd = (event) => {
+  //   console.log("onDragEnd", event);
+  // };
+
   return (
     <div className="p-24 flex gap-5">
       <div className="w-[50%] sticky top-0 h-max">
@@ -44,6 +113,15 @@ const GenerateIndex = (props: Props) => {
         <Button onClick={(e) => onSubmit()} type="button">
           Generate Index
         </Button>
+      </div>
+      <div className="px-12">
+       {/* <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+          <SortableContext items={chapters} strategy={verticalListSortingStrategy}>
+            {chapters.map((chapter) => (
+              <SortableChapter key={chapter} chapter={chapter} />
+            ))}
+          </SortableContext>
+        </DndContext>*/}
       </div>
     </div>
   );
